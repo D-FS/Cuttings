3b7dca86d18a2dce98472b8dd1b667d1b840909f
D-FS <49395324+D-FS@users.noreply.github.com>
2019-04-08 11:56:25 +0200

    Add files via upload

---
+++ bounding_box.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 09:49:37 2019
+
+@author: DFSCHMIDT
+"""
+
+#!/usr/bin/env python3
+
+import numpy as np
+import scipy as scipy
+import basic_functions as bf
+
+def fvolume(angles, coord):
+    """
+    Compute the volume of bouding box of a cloud of points
+    for a given rotation of the cloud of points
+    Needs the rotation angles (rotation angle along x, rotation angle along y) 
+    and the coordinates (3D array) of the cloud of points
+    """
+    theta, phi = angles
+        
+    # cloud of points rotation    
+    M = bf.rotation(theta, phi, 'xy')
+    coord_rot = np.dot(coord, M)
+    
+    # calculation of the side
+    min_x = min(coord_rot[:, 0])
+    max_x = max(coord_rot[:, 0])
+    min_y = min(coord_rot[:, 1])
+    max_y = max(coord_rot[:, 1])
+    min_z = min(coord_rot[:, 2])
+    max_z = max(coord_rot[:, 2])  
+      
+    volume = np.sqrt((max_x-min_x)**2)*np.sqrt((max_y-min_y)**2)*np.sqrt((max_z-min_z)**2)
+    #volume = (max_x-min_x)*(max_y-min_y)*(max_z-min_z)
+    
+    return volume
+
+
+def bbox_optim (coord, initial_guess):
+    """
+    Minimize the volume of the bounding box of a cloud of points using the L-BFGS-B method
+    with respect to the rotation angles of the cloud of points
+    Needs the cloud of points coordinates (3D array) and an initial guess of the rotation angle (rotation angle along x, rotation angle along y)
+    """
+    res = scipy.optimize.minimize(fvolume, initial_guess, args=(coord), method='L-BFGS-B')
+    print(res)
+    return res
+
+
---
+++ plot.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 11:12:03 2019
+
+@author: DFSCHMIDT
+"""
+
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import basic_functions as bf
+
+def radii_histogram (coord):
+    """    
+    Plot the histogram of radii from the ellpsoid test image 
+    Needs its coordinates (3D array)
+    Return the radii histogram plot    
+    """
+    # compute histogram of radii
+    radii = np.sqrt(np.einsum('ai,ai->a', coord, coord)) 
+    hist, bins = np.histogram(radii, bins=100)
+    bins = (bins[1:] + bins[:-1])/2.
+    
+    # plot the histogram graph
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(bins, hist)
+    
+    return plt.show()
+
+
+def bbox_plot (coord_agg, optim_rotation_angle_x, optim_rotation_angle_y, npoints_bbox):
+    """
+    Plots the cloud of points of the aggregate and its bounding box
+    Needs the coordinates of the aggregate (3D array),
+    the optimized rotation angles between x and y and 
+    the number of points in each edge of the bounding box (npoints_bbox)
+    """
+    # plot the cloud of points as a figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    ax.scatter(coord_agg[:, 0], coord_agg[:, 1], coord_agg[:, 2], marker='o', s=0.1)
+    
+    # draw bounding box
+    ROT = bf.rotation(optim_rotation_angle_x, optim_rotation_angle_y, 'xy')
+    coord_rot = np.dot(coord_agg, ROT)
+    
+    min_x = min(coord_rot[:, 0])
+    max_x = max(coord_rot[:, 0])
+    min_y = min(coord_rot[:, 1])
+    max_y = max(coord_rot[:, 1])
+    min_z = min(coord_rot[:, 2])
+    max_z = max(coord_rot[:, 2])
+    
+    x = np.ndarray.tolist(np.linspace(min_x, max_x, num = npoints_bbox))
+    y = np.ndarray.tolist(np.linspace(min_y, max_y, num = npoints_bbox))
+    z = np.ndarray.tolist(np.linspace(min_z, max_z, num = npoints_bbox))
+    
+    MIN_X = np.ndarray.tolist(np.full((npoints_bbox),min_x))
+    MAX_X = np.ndarray.tolist(np.full((npoints_bbox),max_x))
+    MIN_Y = np.ndarray.tolist(np.full((npoints_bbox),min_y))
+    MAX_Y = np.ndarray.tolist(np.full((npoints_bbox),max_y))
+    MIN_Z = np.ndarray.tolist(np.full((npoints_bbox),min_z))
+    MAX_Z = np.ndarray.tolist(np.full((npoints_bbox),max_z))
+    
+    X = x + x + x + x + MIN_X + MAX_X + MIN_X + MAX_X + MIN_X + MAX_X + MIN_X + MAX_X
+    Y = MIN_Y + MAX_Y + MIN_Y + MAX_Y + y + y + y + y + MIN_Y + MIN_Y + MAX_Y + MAX_Y
+    Z = MIN_Z + MIN_Z + MAX_Z + MAX_Z + MIN_Z + MIN_Z + MAX_Z + MAX_Z + z + z + z + z
+    
+    plot_coord = np.dot(np.c_[X,Y,Z], bf.rotation(-optim_rotation_angle_x, -optim_rotation_angle_y, 'yx'))
+    ax.scatter(plot_coord[:, 0], plot_coord[:, 1], plot_coord[:, 2], marker='o', s=1, color='r')
+    #ax.view_init(elev=100., azim=45)
+    
+    return plt.show()
